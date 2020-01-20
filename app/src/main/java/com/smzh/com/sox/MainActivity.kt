9ed3147cddd6250bde92ecee13c4cc43ee1f1a19
liuderong <liuderong@maetimes.com>
2020-01-20 11:27:08 +0800

    sox编译

--- app/src/main/java/com/smzh/com/sox/MainActivity.kt
+++ app/src/main/java/com/smzh/com/sox/MainActivity.kt
@@ -10,6 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, Jni().hello(), Toast.LENGTH_LONG).show()
     }
 }
